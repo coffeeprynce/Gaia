d2adcfdbc92650daa66861d686acd80c77ee6a86
Kyle Nguyen <kylenguyen@kyles-air.mynetworksettings.com>
2023-09-05 17:17:53 -0400

    fixed env key

--- middleware.ts
+++ middleware.ts
@@ -4,14 +4,17 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
+
   // An array of public routes that don't require authentication.
-  publicRoutes: ["/api/webhook/clerk"],
+  publicRoutes: ["/","/api/webhook/clerk"],
 
   // An array of routes to be ignored by the authentication middleware.
   ignoredRoutes: ["/api/webhook/clerk"],
 
-  
+
 });
+
+
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
